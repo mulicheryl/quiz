9754fd548b63a8c224454eec48ceeded49e969eb
mulicheryl <mintpepper13@yahoo.com>
2018-12-13 12:07:46 +0300

    add changes to index.html and scripts.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,70 @@
-// function gradeMe() {
-
-//     var total = 5;
-//     var score = 0;
-
-//     var q1 = document.getElementsByName('q1').value.toString();
-//     var q2 = document.getElementsByName('q2').value.toString();
-//     var q3 = document.getElementsByName('q3').value.toString();
-//     var q4 = document.getElementsByName('q4').value.toString();
-//     var q5 = document.getElementsByName('q5').value.toString();
-
-//     var questionArray = [q1, q2, q3, q4, q5];
-
-//     for (var i = 0; i <= questionArray.length; i++) {
-//         if (questionArray[i] === null || questionArray[i] === '') {
-//             alert("Oops!  You forgot to answer a question.  Please enter an answer for Question " + [i + 1] + ".");
-//             return false;
-//         }
-//     }
-
-
-//     //Set correct Answers
-//     var answers = ["b", "a", "d", "b", "d"];
-
-//     //Check for correct answers
-//     for (var i + 0; i <= total.length; i++) {
-//         if (questionArray[i] === answers[i]) {
-//             score = score + 1;
-//         }
-//     }
-//     alert("Your score is" + score + "out of" + total);
-
-//     return false;
-// }
-
-function gradeMe() {
-    var ansOne = document.querySelector('input[name="q1"]:checked').value;
-    var ansTwo = document.querySelector('input[name="q2"]:checked').value;
-    var ansThree = document.querySelector('input[name="q3"]:checked').value;
-    var ansFour = document.querySelector('input[name="q4"]:checked').value;
-    var ansFive = document.querySelector('input[name="q5"]:checked').value;
-    var correctScore = 0;
-
-
-    if (ansOne === "b") {
-        correctScore += 10;
-    }
-    if (ansTwo === "a") {
-        correctScore += 10;
-    }
-    if (ansThree === "c") {
-        correctScore += 10;
-    }
-    if (ansFour === "d") {
-        correctScore += 10;
-    }
-    if (ansFive === "b") {
-        correctScore += 10;
-    }
-    alert(correctScore);
+var allQuestions = [{
+    question: "What tags contain the JavaScript code?",
+    choices: ["script.", "body.", "link"],
+    correctAnswer: 0
+},
+{
+    question: "Which one is a core language in web pages?",
+    choices: ["ruby.", "java.", "Javascript."],
+    correctAnswer: 2
+},
+{
+    question: " Why is the script tag placed just before the closing body tag?",
+    choices: ["just for fun", "to let the html code run first", "to comply with the standards"],
+    correctAnswer: 1
+},
+{
+    question: "What characters can not be used in variable names? ",
+    choices: ["letters", "numbers", "underscore sign"],
+    correctAnswer: 1
+    
+},
+{
+    question: "What is a floating point number?",
+    choices: [" is always smaller than 0", " is always positive", "is allowed to have a decimal place"],
+    correctAnswer: 2
 }
+]
+var currentquestion = 0;
+var correctAnswers = 0;
+function setupOptions() {
+    $('#question').html(parseInt(currentquestion) + 1 + ". " + allQuestions[currentquestion].question);
+    var options = allQuestions[currentquestion].choices;
+    var formHtml = '';
+    for (var i = 0; i < options.length; i++) {
+        formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"<label for="option' + i + '">' +
+            allQuestions[currentquestion].choices[i] + '</label></div><br/>';
+    }
+    $('#form').html(formHtml);
+    $("#option0").prop('checked', true);
+};
+function checkAns() {
+    if ($("input[name=option]:checked").val() == allQuestions[currentquestion].correctAnswer) {
+        correctAnswers++;
+    };
+};
+$(document).ready(function () {
+    $(".jumbotron").hide();
+    $(".well").hide()
+    $('#start').click(function () {
+        $(".jumbotron").fadeIn();
+        $(".well").fadeIn();
+        $(this).hide();
+    });
+    setupOptions();
+    $("#next").click(function () {
+        event.preventDefault();
+        checkAns();
+        currentquestion++;
+        if (currentquestion < allQuestions.length) {
+            setupOptions();
+            if (currentquestion == allQuestions.length - 1) {
+                $('#next').html("Submit");
+                $('#next').click(function () {
+                    $("#result").html("You correctly answered  " + correctAnswers * 1 + " out of " + currentquestion * 1);
+                    $("#result").fadeIn();
+                });
+            };
+        };
+    });
+});
